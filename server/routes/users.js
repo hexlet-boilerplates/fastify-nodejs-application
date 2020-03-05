047feaf3752819941f7cbad99930805d1f153204
Kirill Mokevnin <mokevnin@gmail.com>
2020-03-04 23:03:36 -0500

    fix forms

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@
 
 // import { User } from '../models';
 // import { buildFromObj, buildFromModel } from '../lib/formObjectBuilder';
+import User from '../entity/User.js';
 
 export default (app) => {
   app
@@ -11,19 +12,22 @@ export default (app) => {
     })
     .get('/users/new', { name: 'newUser' }, async (req, reply) => {
   //     const params = buildFromModel(User.rawAttributes);
-  //     reply.view('users/new', params);
+      const user = {};
+      reply.render('users/new', { user });
     })
-  //   .post('/users', async (req, reply) => {
-  //     const { body: { form } } = req;
-  //     const user = User.build(form);
+    .post('/users', async (req, reply) => {
+      // const { body: { form } } = req;
+      // const user = User.build(form);
+      const user = {};
+      const errors = {};
 
-  //     try {
-  //       await user.save();
-  //       req.flash('info', req.i18n.t('messages.createUserSuccess'));
-  //       reply.redirect(app.reverse('root'));
-  //     } catch (e) {
-  //       const params = buildFromObj(user.dataValues, e.errors);
-  //       reply.view('users/new', params);
-  //     }
-  //   });
+      // try {
+      //   await user.save();
+      //   req.flash('info', req.i18n.t('messages.createUserSuccess'));
+      //   reply.redirect(app.reverse('root'));
+      // } catch (e) {
+      //   const params = buildFromObj(user.dataValues, e.errors);
+      reply.render('users/new', { user, errors });
+      // }
+    });
 };
