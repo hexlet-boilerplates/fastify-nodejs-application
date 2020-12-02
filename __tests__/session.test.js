04f69b9d082d5f1402eb620cfd3218dbf5308889
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-12-02 14:16:53 +0200

    improve session tests

--- __tests__/session.test.js
+++ __tests__/session.test.js
@@ -3,29 +3,28 @@
 import getApp from '../server/index.js';
 import { getTestData, prepareData } from './helpers/index.js';
 
-describe('test users CRUD', () => {
+describe('test session', () => {
   let app;
   let knex;
-  const testData = getTestData();
+  let testData;
 
   beforeAll(async () => {
     app = await getApp();
     knex = app.objection.knex;
     await knex.migrate.latest();
     await prepareData(app);
+    testData = getTestData();
   });
 
-  it('new', async () => {
+  it('test sign in / sign out', async () => {
     const response = await app.inject({
       method: 'GET',
       url: app.reverse('newSession'),
     });
 
     expect(response.statusCode).toBe(200);
-  });
 
-  it('log in', async () => {
-    const response = await app.inject({
+    const responseSignIn = await app.inject({
       method: 'POST',
       url: app.reverse('session'),
       payload: {
@@ -33,33 +32,22 @@ describe('test users CRUD', () => {
       },
     });
 
-    expect(response.statusCode).toBe(302);
-  });
-
-  it('log out', async () => {
-    const response1 = await app.inject({
-      method: 'POST',
-      url: app.reverse('session'),
-      payload: {
-        data: testData.users.existing,
-      },
-    });
-
-    const [sessionCookie] = response1.cookies;
-    const { name, value } = sessionCookie;
-    // вначале логинимся и получаем куки
-    // они понадобятся для запросов на маршруты требующие
+    expect(responseSignIn.statusCode).toBe(302);
+    // после успешной аутентификации получаем куки из ответа,
+    // они понадобятся для выполнения запросов на маршруты требующие
     // предварительную аутентификацию
+    const [sessionCookie] = responseSignIn.cookies;
+    const { name, value } = sessionCookie;
     const cookie = { [name]: value };
 
-    const response2 = await app.inject({
+    const responseSignOut = await app.inject({
       method: 'DELETE',
       url: app.reverse('session'),
       // используем полученные ранее куки
       cookies: cookie,
     });
 
-    expect(response2.statusCode).toBe(302);
+    expect(responseSignOut.statusCode).toBe(302);
   });
 
   afterAll(async () => {
